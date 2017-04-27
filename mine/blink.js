0282972e0601f6a226a92570cc418b2cee4c0130
Pablo Sanchez <psanchez@xpreso.com>
2017-04-27 10:22:32 +0100

    [BKP] - just backing up before installing new linux on desktop

---
+++ mine/blink.js
@@ -0,0 +1,15 @@
+var keyboard = require('../')();
+
+keyboard.color('left', {
+	color: 'red',
+	intensity: 'med'
+});
+keyboard.color('middle', {
+	color: 'green',
+	intensity: 'med'
+});
+keyboard.color('right', {
+	color: 'blue',
+	intensity: 'med',
+});
+
